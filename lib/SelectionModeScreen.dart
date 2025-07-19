86c0431ffa269b60b6099a61f166b6aae81f5323
Anurag-Dubey12 <126979723+Anurag-Dubey12@users.noreply.github.com>
2025-07-19 22:51:04 +0530

    final changes for custom calendar

---
+++ lib/SelectionModeScreen.dart
@@ -0,0 +1,148 @@
+import 'package:dev_to_codes/CustomCalendarView.dart';
+import 'package:dev_to_codes/times_model.dart';
+import 'package:flutter/material.dart';
+
+class SelectionModeScreen extends StatelessWidget {
+  const SelectionModeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, TimesModel> dummyTimeslots = {
+      "Monday": TimesModel(
+        isOpen: true,
+        openTime: "09:00 AM",
+        closeTime: "06:00 PM",
+      ),
+      "Tuesday": TimesModel(
+        isOpen: true,
+        openTime: "09:00 AM",
+        closeTime: "06:00 PM",
+      ),
+      "Wednesday": TimesModel(
+        isOpen: true,
+        openTime: "09:00 AM",
+        closeTime: "06:00 PM",
+      ),
+      "Thursday": TimesModel(
+        isOpen: true,
+        openTime: "09:00 AM",
+        closeTime: "06:00 PM",
+      ),
+      "Friday": TimesModel(
+        isOpen: true,
+        openTime: "09:00 AM",
+        closeTime: "06:00 PM",
+      ),
+      "Saturday": TimesModel(isOpen: true),
+      "Sunday": TimesModel(isOpen: false),
+    };
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text(
+          'Selection Mode',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                ((e) => CustomCalendarView(multiDay: false)),
+                          ),
+                        ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: Text('Show Single Day'),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                ((e) => CustomCalendarView(multiDay: true)),
+                          ),
+                        ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: Text('Show Multiple Days'),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                ((e) => CustomCalendarView(
+                                  multiDay: true,
+                                  timeslots: dummyTimeslots,
+                                )),
+                          ),
+                        ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: Text('Show With TimeSlots'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dev_to_codes/SelectionModeScreen.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Calender',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: SelectionModeScreen(),
+    );
+  }
+}
